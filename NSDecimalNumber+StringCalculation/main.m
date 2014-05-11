1ecea19b10ad644d54322c03e0e61af4303d82f8
Adi Li <ctlihk@live.hk>
2014-05-11 23:13:39 +0800

    Initial Commit

---
+++ NSDecimalNumber+StringCalculation/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  NSDecimalNumber+StringCalculation
+//
+//  Created by Adi Li on 11/5/14.
+//  Copyright (c) 2014 Adi Li. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
